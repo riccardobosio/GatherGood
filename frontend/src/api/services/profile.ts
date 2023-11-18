27be978c5a7ae030b736b50c74e18434b686d641
Laia Vallès <laia@sparsity-technologies.com>
2023-11-18 11:53:08 +0100

    Create profile page with user details

---
+++ frontend/src/api/services/profile.ts
@@ -0,0 +1,32 @@
+
+import {AxiosResponse} from "axios";
+import {api} from "../axios";
+import AbstractService from "./abstract.js";
+import {User} from "../types";
+
+class ProfileService extends AbstractService<User> {
+
+    async get() {
+        const response: AxiosResponse<User> = await api.get(`${this.endpoint}/`);
+        return response.data;
+    }
+
+    async update(user: Partial<User>) {
+        const response: AxiosResponse = await api.patch(`${this.endpoint}/`, user);
+        return response;
+    }
+
+    async deleteAccount(password: { password: string; }) {
+        const response: AxiosResponse = await api.delete(`${this.endpoint}/`, {data: password});
+        return response;
+    }
+
+    async sendDataByEmail() {
+        const response: AxiosResponse = await api.post(`${this.endpoint}/personal-data-export/`);
+        return response;
+    }
+
+}
+
+const profileService = new ProfileService("/profile");
+export default profileService;
