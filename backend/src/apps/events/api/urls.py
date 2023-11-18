750dc42a11262da1abad544c6b1d1043c8bb6378
Riccardo Bosio <riccardobosio18@gmail.com>
2023-11-18 11:51:09 +0100

    Add endpoints to get list of joined and created events

--- backend/src/apps/events/api/urls.py
+++ backend/src/apps/events/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from apps.events.api.views.event import EventsAPI, EventDetailRetrieveUpdateDestroyAPI
+from apps.events.api.views.event import EventsAPI, EventDetailRetrieveUpdateDestroyAPI, EventsJoinedAPI, EventsCreatedAPI
 
 urlpatterns = [
     path('events/', EventsAPI.as_view(), name='events'),
     path('events/<uuid:pk>/', EventDetailRetrieveUpdateDestroyAPI.as_view(), name='events_id'),
+    path('events/joined/', EventsJoinedAPI.as_view(), name='events_joined'),
+    path('events/created/', EventsCreatedAPI.as_view(), name='events_created'),
 ]
