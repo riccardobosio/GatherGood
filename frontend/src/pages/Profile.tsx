50e91b76d295f088c23958d32d6418b3eca45dd0
Laia Vallès <laia@sparsity-technologies.com>
2023-11-18 13:03:19 +0100

    Move profile to tab

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,8 +1,19 @@
 // src/pages/UserProfilePage.tsx
 import React, {useEffect, useState} from 'react';
-import { IonPage, IonContent, IonItem, IonLabel, IonInput, IonButton } from '@ionic/react';
+import {
+    IonPage,
+    IonContent,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonHeader,
+    IonToolbar,
+    IonTitle, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent
+} from '@ionic/react';
 import { User } from '../api/types';
 import {useUser} from "../hooks/useUser";
+import {formatDate} from "../utils/date";
 
 const UserProfilePage: React.FC = () => {
     const { user, getUser } = useUser();
@@ -22,31 +33,33 @@ const UserProfilePage: React.FC = () => {
 
     return (
         <IonPage>
-            <IonContent className="ion-padding center-content">
-                <div className="form-container">
-                    <h2>User Profile</h2>
-                    <IonItem>
-                        <IonLabel>Email</IonLabel>
-                        <IonInput value={editedUser?.email} disabled />
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel>First Name</IonLabel>
-                        <IonInput
-                            value={editedUser?.first_name}
-                            onIonChange={(e) => handleInputChange('first_name', e.detail.value!)}
-                        />
-                    </IonItem>
-                    <IonItem>
-                        <IonLabel>Last Name</IonLabel>
-                        <IonInput
-                            value={editedUser?.last_name}
-                            onIonChange={(e) => handleInputChange('last_name', e.detail.value!)}
-                        />
-                    </IonItem>
-                    <IonButton expand="block" onClick={handleSave}>
-                        Save Changes
-                    </IonButton>
-                </div>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Profile</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+            <IonItem>
+                <IonLabel>Email</IonLabel>
+                <IonInput value={editedUser?.email} disabled/>
+            </IonItem>
+            <IonItem>
+                <IonLabel>First Name</IonLabel>
+                <IonInput
+                    value={editedUser?.first_name}
+                    onIonChange={(e) => handleInputChange('first_name', e.detail.value!)}
+                />
+            </IonItem>
+            <IonItem>
+                <IonLabel>Last Name</IonLabel>
+                <IonInput
+                    value={editedUser?.last_name}
+                    onIonChange={(e) => handleInputChange('last_name', e.detail.value!)}
+                />
+            </IonItem>
+            <IonButton expand="block" onClick={handleSave}>
+                Save Changes
+            </IonButton>
             </IonContent>
         </IonPage>
     );
