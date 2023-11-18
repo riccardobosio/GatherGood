05c4f35ddbe64048e1261b86f5eb09e774aa97ce
arystanigen <arystan@sparsity-technologies.com>
2023-11-18 12:29:48 +0100

    Add registration

--- backend/src/apps/accounts/api/views/user.py
+++ backend/src/apps/accounts/api/views/user.py
@@ -5,7 +5,11 @@ from rest_framework.response import Response
 from apps.accounts.api.views.common import CustomApiView
 from apps.accounts.models import User
 from apps.accounts.exceptions import PasswordNotValidError
-from apps.accounts.serializers import ProfileDeleteInputSerializer, UserDetailSerializer
+from django.shortcuts import get_object_or_404
+from apps.accounts.serializers import ProfileDeleteInputSerializer, UserDetailSerializer, UserReviewDetailSerializer, \
+    RegistrationInputSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import ValidationError
 
 
 class ProfileRetrieveUpdateDeleteAPI(
@@ -62,3 +66,48 @@ class ProfileRetrieveUpdateDeleteAPI(
         response = Response(status=status.HTTP_204_NO_CONTENT)
         unset_jwt_cookies(response)
         return response
+
+
+class UserDetailAPI(CustomApiView):
+    api_description = 'User Detail and Review'
+
+    @swagger_auto_schema(
+        operation_summary='Get User Detail.',
+        operation_description=api_description,
+        operation_id='GetUserDetail',
+        responses={status.HTTP_200_OK: UserReviewDetailSerializer()}
+    )
+    def get(self, request, user_id):
+        fetched_user: User = get_object_or_404(User, id=user_id)
+        serializer = UserReviewDetailSerializer(fetched_user)
+        return Response(serializer.data)
+
+
+class RegistrationApi(CustomApiView):
+    api_description = 'User Registration'
+    permission_classes = (AllowAny,)
+    authentication_classes = ()
+
+    @swagger_auto_schema(
+        operation_summary="Registration",
+        operation_description=api_description,
+        operation_id="Registration",
+        request_body=RegistrationInputSerializer,
+        responses={status.HTTP_200_OK: UserDetailSerializer()}
+    )
+    def post(self, request):
+        serializer = RegistrationInputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.data
+        if data["password"] != data["password_confirm"]:
+            raise ValidationError("Both password and password_confirmation are required.")
+
+        del data["password_confirm"]
+
+        User.objects.create_user(
+            **data
+        )
+
+        return Response(
+            status=status.HTTP_200_OK
+        )
